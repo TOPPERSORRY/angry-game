7cd3e848b32b92cc6fe4c04c6fa0fe9e9827ca60
TOPPERSORRY <ritvikjain2008@gmail.com>
2021-03-20 17:49:54 +0530

    added log6

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,7 +36,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log6 = new Log(400,400,400,400);
 }
 
 function draw(){
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
 }
